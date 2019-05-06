cd31d61815f8f0c74ec112d46de36d4f0037a7b2
nellidimitrowa <nellidimitrowa@gmail.com>
2019-05-06 13:20:27 +0300

    Creating, writing and saving file

--- FinanceManager.c
+++ FinanceManager.c
@@ -1,8 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *fileName() {
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	char *months[12] = {"january", "february", "march", "april", "may", "june", 
+				"july", "august", "september", "october", "november", "december"};
+	int year = tm.tm_year + 1900;
+
+	char stringYear[32];
+	sprintf(stringYear, "%d", year);
+
+	char *filename = malloc(strlen(months[tm.tm_mon]) + 5);
+	strcpy(filename, months[tm.tm_mon]);
+    strcat(filename, stringYear);
+    return filename;
+}
+
+void saveFile() {
+	char *storagePath = "D:\\WORD\\UNI\\semester6\\SPr\\TASK\\FinanceManager\\FinanceManagement\\Storage\\";
+	char *filename = fileName();
+	char *filePath = malloc(strlen(storagePath) + strlen(filename) + strlen(".txt") + 1);
+	strcpy(filePath, storagePath);
+	strcat(filePath, filename);
+	strcat(filePath, ".txt");
+	FILE *f = fopen(filePath, "w");
+
+	const char *text = "Write this to the file";
+	fprintf(f, "Some text: %s\n", text);
+
+	fclose(f);
+}
+
+
+// TODO : enterNewCosts()
+// 		: checkFilename()
 
 void choiceAction(int choice) {
 	if (choice == 1) {
 		printf("You chose 1.\n");
+		saveFile();
 	} else if (choice == 2) {
 		printf("You chose 2.\n");
 	} else if(choice == 3) {
